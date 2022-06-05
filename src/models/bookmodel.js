01a58da63ffbb344e8fcef8c7c49cf26fe65f41a
ajinkya1507 <ajinkya.bh.eco@gmail.com>
2022-06-05 13:49:00 +0530

    ssdsad

--- src/models/bookmodel.js
+++ src/models/bookmodel.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String,
+    bookName: {
+        type: String,
+        unique: true,
+        required:true
+    },
     authorName: String,
-    genre: String,
+
+    genre: [String],
 }, { timestamps: true });
 
 module.exports = mongoose.model('Book1', bookSchema) //users
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ const bookcontroller = require("../controllers/bookcontroller")
 
 
 
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -17,5 +18,7 @@ router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBookUser", bookcontroller.createBookUser)
 router.get("/getbookdetails", bookcontroller.getBooksData)
+
+
 
 module.exports = router;
